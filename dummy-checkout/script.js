02715bce4908c92247d98780f93cd55f94062def
roopeshrai05 <rupesh@pranstech.com>
2026-02-23 06:56:11 -0500

    Built multi-product checkout flow with cart and UI

--- dummy-checkout/script.js
+++ dummy-checkout/script.js
@@ -1,5 +1,52 @@
-document.getElementById("checkoutForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("successMessage").innerText =
-    "Order placed successfully!";
-});
+function addToCart(name, price) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({ name, price });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Added to cart!");
+}
+
+function loadCart() {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let cartDiv = document.getElementById("cartItems");
+  let total = 0;
+
+  if (!cartDiv) return;
+
+  cartDiv.innerHTML = "";
+
+  cart.forEach((item, index) => {
+    total += item.price;
+
+    cartDiv.innerHTML += `
+      <div>
+        ${item.name} - $${item.price}
+        <button onclick="removeItem(${index})">Remove</button>
+      </div>
+      <hr/>
+    `;
+  });
+
+  document.getElementById("totalAmount").innerText =
+    "Total: $" + total;
+}
+
+function removeItem(index) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  loadCart();
+}
+
+if (document.getElementById("cartItems")) {
+  loadCart();
+}
+
+if (document.getElementById("checkoutForm")) {
+  document.getElementById("checkoutForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    localStorage.removeItem("cart");
+    window.location.href = "success.html";
+  });
+}
